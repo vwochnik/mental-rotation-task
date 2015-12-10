96021f15629b4ccb787af5d6806a90859ed352db
Vincent Wochnik <v.wochnik@gmail.com>
2015-12-10 14:18:17 +0100

    first window

--- mrt/__main__.py
+++ mrt/__main__.py
@@ -1,9 +1,14 @@
 from __init__ import *
 
 def main():
-    print 'hello'
-    inst = Main(640, 480)
-    inst.loop
+    inst = Main(640, 480, "Mental Rotation Task")
+    inst.init()
+    try:
+        inst.loop()
+    except Exception, e:
+        tb = sys.exc_info()[2]
+        traceback.print_exception(e.__class__, e, tb)
+    inst.quit()
 
 if __name__ == '__main__':
     main()
--- mrt/main.py
+++ mrt/main.py
@@ -1,7 +1,23 @@
+import pygame, time
+#from VideoCapture import Device
+
 class Main:
-    def __init__(self, width, height):
+    def __init__(self, width, height, caption):
         self.width = width
         self.height = height
+        self.caption = caption
 
-    def loop():
-        print 'hello'
+    def init(self):
+        pygame.init()
+        pygame.display.set_caption(self.caption)
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+    def quit(self):
+        pygame.quit()
+
+    def loop(self):
+        running = True
+        while running:
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT:
+                running = False
